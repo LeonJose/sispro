9071bf4517aa7dfe2f5fc453a6d850c594ad10da
LeonJose <joseluisleondamas@gmail.com>
2016-04-08 20:30:50 -0500

    Correccion error de prueba

--- SisPro/SisPro/Impresora.cs
+++ SisPro/SisPro/Impresora.cs
@@ -75,7 +75,7 @@ namespace SisPro
             SqlCommand comandosql = new SqlCommand(instruccion);
             comandosql.Parameters.Add(new SqlParameter("@nom", _nombre));
             comandosql.Parameters.Add(new SqlParameter("@id",_idimpresora));
-            return EjecutarComando(comandosql);jgfjhgfhjfjhkghgvjc
+            return EjecutarComando(comandosql);
         }
 
         #endregion
