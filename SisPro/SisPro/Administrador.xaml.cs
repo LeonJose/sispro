d5ad254b140948db9e95708b14799e5b76c4c152
trelloch <jlpq.tic@gmail.com>
2016-04-14 15:42:58 -0700

    descargando proyecto

---
+++ SisPro/SisPro/Administrador.xaml.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+
+namespace SisPro
+{
+    /// <summary>
+    /// Lógica de interacción para Administrador.xaml
+    /// </summary>
+    public partial class Administrador : Window
+    {
+        public Administrador()
+        {
+            InitializeComponent();
+            headerlogo();
+            txtnombre.Focus();
+        }
+        private void headerlogo()
+        {
+            Image _image = new Image();
+            BitmapImage _bi = new BitmapImage();
+            _bi.BeginInit();
+            _bi.UriSource = new System.Uri("pack://application:,,,/Recursos/imagenes/1.png");
+            _bi.EndInit();
+            _image.Source = _bi;
+            ImageBrush _ib = new ImageBrush();
+            _ib.ImageSource = _bi;
+            rt_admin.Fill = _ib;
+        }
+    }
+}
---
+++ SisPro/SisPro/Listados.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data;
+
+namespace SisPro
+{
+    public class Listados: Conexion
+    {
+        public static int ObtenerNumero()
+        {
+            int i = 0;
+            string consulta = "select count(esp_id) as cont from espera where esp_fecha= '"+DateTime.Now.ToString("yyyy-MM-dd")+"'";
+            System.Data.DataRow registro = LeerRegistro(consulta);
+            if(registro !=null)
+            {
+                i = int.Parse(registro["cont"].ToString())+1;
+            }
+
+            return i;
+        }
+
+        public static List<Departamento> Departamentos()
+        {
+            List<Departamento> depto = new List<Departamento>();
+            string consulta = "SELECT dep_id from departamentos";
+            DataTable tabla = LeerTabla(consulta);
+            foreach (DataRow registro in tabla.Rows)
+            {
+                depto.Add(new Departamento(int.Parse(registro["dep_id"].ToString())));
+            }
+            return depto;
+        }
+    }
+}
